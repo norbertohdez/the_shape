bf7ec1a1764f8a136da7247a409bffc254a3a3dc
Norberto Hdez <norbertohdez@gmail.com>
2017-04-16 00:54:50 -0700

    Add overlay as component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
+import Overlay from '../Overlay/Overlay';
 
 
 /* CORE -------------------------------------- */
@@ -20,6 +21,7 @@ class Page extends Component {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
+        <Overlay />
       </div>
     );
   }
